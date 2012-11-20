58223eaa55440fe82b0e400dc9b241a6fc1791e3
sofika <fischer@sofika.de>
2012-11-20 10:15:20 +0100

    mm

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,7 @@ class HelloWorld
 	public static void main (String[] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Hello World Projekt3");
-		System.out.println("Das ist eine Aenderung");
+		System.out.println("Das ist der master");
 
 	}
 }
