79482204c488ffc7f3c9c0599df3f45afcd87832
sofika <fischer@sofika.de>
2012-11-20 10:50:51 +0100

    hh

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ class HelloWorld
 	public static void main (String[] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Das ist der master");
+		System.out.println("Das ist branch2");
+		System.out.println("Das soll gepusht werden");
 
 	}
 }
