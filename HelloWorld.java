1cdc472be43316ca0280b86268a17e3781a86085
sofika <fischer@sofika.de>
2012-11-20 15:25:31 +0100

    ii

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld
 		System.out.println("Hello World");
 		System.out.println("Das ist branch2");
 		System.out.println("Das soll gepusht werden2");
+		System.out.println("Das soll gepusht werden3");
 
 	}
 }
