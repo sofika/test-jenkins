70e4be4e69b71a75a2c8e23ac5e61e2b3eeb93fa
sofika <fischer@sofika.de>
2012-12-03 18:28:39 +0100

    i

--- User.php
+++ User.php
@@ -1,11 +1,44 @@
 <?php
+/**
+ * Eine Klasse für die Demonstration von phpDocumentor.
+ *
+ * <code>
+ * <?php
+ * require_once 'Klasse.php';
+ *
+ * $objekt = new Klasse;
+ * $objekt->setzeVariable('test');
+ * ?>
+ * </code>
+ *
+ * @author      Klaus Fischer <fischer@sofika.de>
+ * @package     packageuser
+ * @subpackage  user1
+ * @filesource
+ */
 class User {
-    // Eine kleine 
+    /**
+    * name
+    * @var  string
+    * @access private;
+    */
     protected $name;
 
+/**
+ * get the name of a user
+ *
+ * @return name
+ *
+ */
     public function getName() {
         return $this->name;
     }
+/**
+* set name
+*
+* @param string $name name to be set
+* @access public
+*/
 
     public function setName($name) {
         $this->name = $name;
