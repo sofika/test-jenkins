d1c54dd301362edf204cbb8e8e5205d11cb09b06
sofika <fischer@sofika.de>
2012-12-04 12:36:50 +0100

    commited

---
+++ script/deploy_crm_staging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+# deploy crm to testing environment---------------------------------------
+# ------------------------------------------------------------------------
+echo "start deploying to testing enviornment....."
+echo "finished deploying"
